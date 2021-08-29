93656d894f7fa5eaf96b3aa0051a132b33fab105
tabatii <76856660+tabatii@users.noreply.github.com>
2021-08-29 19:56:41 +0100

    initial commit

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 export default function ({app, env, $axios}) {
-	$axios.setBaseURL(env.API_URL)
+	$axios.setBaseURL(env.API_URL || process.env.API_URL)
 	$axios.onRequest(config => {
 		if (app.$cookies.get('ut'))
 			$axios.setToken(app.$cookies.get('ut'), 'Bearer')
