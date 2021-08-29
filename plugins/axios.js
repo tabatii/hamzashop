c268eab8d7e98a3dfe4f84db528af9bb94cc4491
tabatii <76856660+tabatii@users.noreply.github.com>
2021-08-29 19:29:35 +0100

    initial commit

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,5 @@
 export default function ({app, env, $axios}) {
-	$axios.setBaseURL(env.API_URL)
+	$axios.setBaseURL('https://hamzashopapi.herokuapp.com/api')
 	$axios.onRequest(config => {
 		if (app.$cookies.get('ut'))
 			$axios.setToken(app.$cookies.get('ut'), 'Bearer')
