15188e9ab8abc69ee3fde0c6b58770939ba12c80
tabatii <76856660+tabatii@users.noreply.github.com>
2021-09-09 03:25:35 +0100

    initial commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,7 +76,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/styles/main.css',
+    '~/assets/main.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
